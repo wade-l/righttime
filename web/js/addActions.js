9a1c8d4c6bbc4295b62f4be99728a6798ddd67d7
Wade Lahoda <wade.lahoda@gmail.com>
2018-03-21 17:00:19 -0600

    Added javascript to allow you to insert actions into a turn --WadeL

---
+++ web/js/addActions.js
@@ -0,0 +1,54 @@
+// Adapted from https://symfony.com/doc/3.4/form/form_collections.html
+
+
+var $collectionHolder;
+
+// setup an "add an action" link
+var $addActLink = $('<a href="#" class="add_act_link">Add an action</a>');
+var $newLinkLi = $('<li></li>').append($addActLink);
+
+jQuery(document).ready(function() {
+    // Get the ul that holds the collection of actions
+    console.log("blah")
+    $collectionHolder = $('ul.acts');
+
+    // add the "add an action" anchor and li to the actions ul
+    $collectionHolder.append($newLinkLi);
+
+    // count the current form inputs we have (e.g. 2), use that as the new
+    // index when inserting a new item (e.g. 2)
+    $collectionHolder.data('index', $collectionHolder.find(':input').length);
+
+    $addActLink.on('click', function(e) {
+        // prevent the link from creating a "#" on the URL
+        e.preventDefault();
+
+        // add a new action form (see next code block)
+        addActForm($collectionHolder, $newLinkLi);
+    });
+});
+
+function addActForm($collectionHolder, $newLinkLi) {
+    // Get the data-prototype explained earlier
+    var prototype = $collectionHolder.data('prototype');
+
+    // get the new index
+    var index = $collectionHolder.data('index');
+
+    var newForm = prototype;
+    // You need this only if you didn't set 'label' => false in your tags field in TaskType
+    // Replace '__name__label__' in the prototype's HTML to
+    // instead be a number based on how many items we have
+    // newForm = newForm.replace(/__name__label__/g, index);
+
+    // Replace '__name__' in the prototype's HTML to
+    // instead be a number based on how many items we have
+    newForm = newForm.replace(/__name__/g, index);
+
+    // increase the index with one for the next item
+    $collectionHolder.data('index', index + 1);
+
+    // Display the form in the page in an li, before the "Add a tag" link li
+    var $newFormLi = $('<li></li>').append(newForm);
+    $newLinkLi.before($newFormLi);
+}
