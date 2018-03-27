99c511f55849ca8ed77d44f90d34e7cc8619e560
Wade Lahoda <wade.lahoda@gmail.com>
2018-03-27 17:05:29 -0600

    Working on getting action deletion working. --Wade L

--- web/js/addActions.js
+++ web/js/addActions.js
@@ -9,17 +9,15 @@ var $newLinkLi = $('<li></li>').append($addActLink);
 
 jQuery(document).ready(function() {
     // Get the ul that holds the collection of actions
-    console.log("blah")
     $collectionHolder = $('ul.acts');
+
+    // add a delete link to all of the existing action form li elements
+    $collectionHolder.find('li.delete_act').each(function() {
+        addActFormDeleteLink($(this));
+    })
 
     // add the "add an action" anchor and li to the actions ul
     $collectionHolder.append($newLinkLi);
-
-    // add a delete link to all of the existing action form li elements
-    $collectionHolder.find('li').each(function() {
-        addActFormDeleteLink($(this));
-    })
-    
 
     // count the current form inputs we have (e.g. 2), use that as the new
     // index when inserting a new item (e.g. 2)
@@ -56,6 +54,7 @@ function addActForm($collectionHolder, $newLinkLi) {
 
     // Display the form in the page in an li, before the "Add a tag" link li
     var $newFormLi = $('<li></li>').append(newForm);
+    addActFormDeleteLink($newFormLi);
     $newLinkLi.before($newFormLi);
 
 }
